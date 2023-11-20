faee46c8c18410ee9f66eb51db2fbad6aed91be3
David Wagemann <dawag2001@gmail.com>
2023-11-20 15:20:48 +0100

    added notebook for preparing the data

--- src/dataset/trizod_scores/parse.py
+++ src/dataset/trizod_scores/parse.py
@@ -7,13 +7,16 @@ import polars as pl
 
 def read_reference_json(filepath: Union[Path, str]) -> pl.DataFrame:
     lines = open(filepath).readlines()
-    raw_json = [*[line + ',' for line in lines[:-1]], lines[-1]]
-    raw_json.insert(0, '[')
-    raw_json.append(']')
-    raw_json = ''.join(raw_json)
+    raw_json = [*[line + "," for line in lines[:-1]], lines[-1]]
+    raw_json.insert(0, "[")
+    raw_json.append("]")
+    raw_json = "".join(raw_json)
 
-    df = pl.from_records(json.loads(''.join(raw_json)))
-    return df.select(df.columns[:23])  # return just the columns we don't have in the CSV file
+    df = pl.from_records(json.loads("".join(raw_json)))
+    return df.select(
+        df.columns[:23]
+    )  # return just the columns we don't have in the CSV file
+
 
 def read_score_csv(filepath: Union[Path, str]) -> pl.DataFrame:
     return pl.read_csv(filepath, dtypes={"HA": pl.Float64, "HB": pl.Float64})
