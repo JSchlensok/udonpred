e955a2426864b0a8fb5e4f433b62ae4f38fb4a46
Julius Schlensok <julius@schlensok.org>
2024-03-11 14:28:02 +0100

    Housekeeping

--- src/pipeline/caid/run.py
+++ src/pipeline/caid/run.py
@@ -1,6 +1,5 @@
-# TODO remove unused imports
 import datetime
-import yaml
+from collections import OrderedDict
 from datetime import datetime as dt
 from pathlib import Path
 from typing_extensions import Annotated
@@ -11,6 +10,7 @@ import polars as pl
 import rich.progress as progress
 import torch
 import typer
+import yaml
 from Bio import SeqIO
 
 from src.models import FNN
@@ -26,28 +26,33 @@ def write_caid_file(directory: Path, protein_id: str, scores: torch.tensor, sequ
 # TODO try multithreading using joblib
 def main(
     fasta_file: Annotated[Path, typer.Option("--input-fasta", "-i")],
-    embedding_file: Annotated[Path, typer.Option("--embedding-file", "-e")] = None
+    embedding_file: Annotated[Path, typer.Option("--embedding-file", "-e")] = None,
+    model_dir: Annotated[Path, typer.Option("--model-directory")] = None,
+    output_dir: Annotated[Path, typer.Option("--output-directory", "-o")] = None
 ):
     script_start_time = dt.now()
+    
+    model_dir = model_dir or Path.cwd() / "model"
+    output_dir = output_dir or Path.cwd() / "out"
 
-    # Load model
     device = "cpu"
     embedding_type = "prostt5"
     method_name = "FNN"
     embedding_dim = embedding_dimensions[embedding_type]
 
-    model_dir = Path.cwd() / "model"
     model_config = yaml.safe_load((model_dir / "config.yml").open())
     
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     map_location = torch.device("cpu") if not torch.cuda.is_available() else None
-    model = FNN(n_features=embedding_dim, **params)
+    model = FNN(n_features=embedding_dim, **model_config["model"]["params"])
     model.load_state_dict(torch.load(model_dir / "model.pt", map_location=map_location))
+    model = model.double()
 
     # Prepare file output
-    output_path = Path.cwd() / "disorder"
-    output_path.mkdir(exist_ok=True, parents=True)
+    disorder_output_path = output_dir / "disorder"
+    disorder_output_path.mkdir(exist_ok=True, parents=True)
 
-    sequences = {rec.id: rec.seq for rec in SeqIO.parse(fasta_file, "fasta")}
+    sequences = OrderedDict((rec.id, rec.seq) for rec in SeqIO.parse(fasta_file, "fasta"))
     execution_times = []
 
     with progress.Progress(
@@ -65,7 +70,7 @@ def main(
             emb = emb.clone().to(device=device, dtype=torch.float64)
             pred = model(emb)
 
-            write_caid_file(output_path, id, pred, sequences[id])
+            write_caid_file(disorder_output_path, id, pred, sequences[id])
             end_time = dt.now()
             execution_time = end_time - start_time
             execution_times.append({"sequence": id, "milliseconds": execution_time / datetime.timedelta(milliseconds=1)})
@@ -73,7 +78,7 @@ def main(
             pbar.advance(overall_progress)
 
         csv_lines = pl.from_records(execution_times).write_csv(file=None)
-        with open("timings.csv", "w+") as f:
+        with open(output_dir / "timings.csv", "w+") as f:
             f.write(f"# Running test, started {script_start_time:%c}\n")
             f.write(csv_lines)
 
